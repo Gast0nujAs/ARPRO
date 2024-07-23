82650b6dd18e76c05c5d304cf12c17900d143f13
Gaston Hernandez <hcesargaston@gmail.com>
2024-07-23 08:43:26 -0300

    ult cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
 import menuBurguer from '../assets/img/menuBurguer.png'
+import { useNavigate, Navigate } from 'react-router-dom'
+import { useState } from 'react'
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+
+        setOpen(!open)
+    }
+
+    const navigate = useNavigate();
+
+   
+    
+
     return (
 
         <>
-            <ul className='hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-4
-     sm:items-center '>
+            <ul className= {`${open ? "flex" : "hidden"}  bg-white w-[100%] h-[200px] flex-col items-center place-content-around mx-auto
+            
+            
+                sm:flex sm:flex-row text-black text-[18px] sm:w-[438px] sm:place-content-around sm:text-4
+                sm:items-center sm:gap-10 sm:mx-[20px]`}>
                 <li>
-                    <a href="/" className='hover:text-SoftOrange sm:hover:text-[20px]'>Home</a>
+                    <a href="/" className='hover:text-SoftOrange sm:hover:text-[20px] '>Home</a>
                 </li>
                
                 <li>
-                    <a href="/sobre-nosotros" className='hover:text-SoftOrange sm:hover:text-[20px]'>Nosotros</a>
+                    <a onClick={() => navigate('/sobre-nosotros')} className='hover:text-SoftOrange sm:hover:text-[20px] hover:cursor-pointer'>Nosotros</a>
                 </li>
                 <li>
-                    <a href="/contacto" className='hover:text-SoftOrange sm:hover:text-[20px]'>Contacto</a>
+                    <a onClick={() => navigate('/contacto')}  className='hover:text-SoftOrange sm:hover:text-[20px] hover:cursor-pointer'>Contacto</a>
                 </li>
             </ul>
-            <img className='w-10 h-4 sm:hidden cursor-pointer' src={menuBurguer} alt='menu'/>
+            <img className='w-10 h-4 sm:hidden cursor-pointer' src={menuBurguer} alt='menu' onClick={handleClick}/>
 
         </>
 
