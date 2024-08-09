e6bf868006d44e981a72ba8e21ee70f2a9444701
Gaston Hernandez <hcesargaston@gmail.com>
2024-08-09 12:32:48 -0300

    asd3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logoArpro from "../assets/logo-arpro/arpro-Logo.jpeg"
 
 export function Header() {
     return (
-        <header className="flex mb-4 mt-2 mx-4 w-full bg-white place-content-between items-center ">
+        <header className="flex mb-4 mt-2 sm:mx-2 w-full bg-white place-content-between items-center ">
             <img src={logoArpro} alt="logo" className="w-[90px] h-[90px] sm:w-[160px] sm:h-[150px]"/>
             <Navbar />
         </header>
